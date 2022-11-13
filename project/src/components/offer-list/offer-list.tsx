696148e344df98c41b85584602b79a80548296d0
sukhoverkhova <sukhoverkhova.an@gmail.com>
2022-11-13 17:34:53 +0300

    Добавляю компонент карты на страницу просмотра предложения

--- project/src/components/offer-list/offer-list.tsx
+++ project/src/components/offer-list/offer-list.tsx
@@ -3,14 +3,27 @@ import {Offer} from '../../types/offer';
 
 type Props = {
   offers: Offer[];
+  className?: string;
+  imageWrapperClassName?: string;
+  imageWidth?: number;
+  imageHeight?: number;
 };
 
 function OfferList(props: Props): JSX.Element {
-  const {offers} = props;
+  const {offers, className, imageWrapperClassName, imageWidth, imageHeight} = props;
 
   return(
     <>
-      {offers.map((offer) => <OfferItem offer={offer} key={offer.id}/> )}
+      {offers.map((offer) => (
+        <OfferItem
+          offer={offer}
+          key={offer.id}
+          className={className}
+          imageWrapperClassName={imageWrapperClassName}
+          imageWidth={imageWidth}
+          imageHeight={imageHeight}
+        />
+      ))}
     </>
   );
 }
